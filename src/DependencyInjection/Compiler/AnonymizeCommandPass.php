59b322d318678ccc0bfd167887c43486dcb660da
Smaine Milianni <smaine.milianni@gmail.com>
2021-03-29 18:34:16 +0100

    add namespace

--- src/DependencyInjection/Compiler/AnonymizeCommandPass.php
+++ src/DependencyInjection/Compiler/AnonymizeCommandPass.php
@@ -7,6 +7,7 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
+use WebnetFr\DatabaseAnonymizer\Anonymizer;
 use WebnetFr\DatabaseAnonymizer\ConfigGuesser\ConfigGuesser;
 use WebnetFr\DatabaseAnonymizerBundle\Command\AnonymizeCommand;
 use WebnetFr\DatabaseAnonymizerBundle\Config\AnnotationConfigFactory;
